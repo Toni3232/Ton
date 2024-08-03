97c0804b34d06d554a36a27027cdc51eb94c2658
Joni Gain <Tonigain26@gmail.com>
2024-08-03 02:36:53 +0000

    feat: initiated the project

--- contracts/PrivateNFT.sol
+++ contracts/PrivateNFT.sol
@@ -8,7 +8,7 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract PrivateNFT is ERC721, ERC721Burnable, Ownable {
     constructor(address initialOwner)
-        ERC721("to","tn")
+        ERC721("gg","fan")
         Ownable(initialOwner)
     {}
 
